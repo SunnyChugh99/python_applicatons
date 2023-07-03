909acea75327f3d93212b3f9b98a6cc46fae646f
Sunny Chugh <10683796@ltilbom20401298.lntinfotech.com>
2023-07-03 13:23:25 +0530

    mosaic-ai user for vscode

---
+++ streamlit/hello.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def main():
+    st.title("Hello World!")
+    st.title("Hello World!")
+
+  
+    st.write("Welcome to your first Streamlit app.")
+
+if __name__ == "__main__":
+    main()
